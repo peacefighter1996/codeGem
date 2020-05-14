0c61a35701a90132a532b9b969e25c91bb808a35
Zander van der steege <zandervds@gmail.com>
2020-05-14 22:29:44 +0200

    First working program csv parser

--- Parser/Mechanoid.Parser.CSV/CsvParser.cs
+++ Parser/Mechanoid.Parser.CSV/CsvParser.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace Mechanoid.Parser.CSV
@@ -17,9 +22,45 @@ namespace Mechanoid.Parser.CSV
             throw new NotImplementedException();
         }
 
-        public T[] ParseMultiObjectDocument(string filepath)
+        public T[] ParseMultiObjectDocument(string filePath)
         {
-            throw new NotImplementedException();
+            var stream = new StreamReader(filePath);
+            List<T> resultList = new List<T>();
+            var row = string.Empty;
+            bool firstrow = true;
+            do
+            {
+                row = stream.ReadLine();
+                if (!string.IsNullOrWhiteSpace(row)&& !firstrow)
+                {
+                    var DataType = typeof(T);
+                    var obj = typeof(T).GetConstructor(Type.EmptyTypes)?.Invoke(null);
+                    if (obj == null) throw (new InvalidCastException());
+                    var properties = DataType.GetProperties();
+                    string[] objects = row.Split(',');
+                    for (int i = 0; i < objects.Length-1; i++)
+                    {
+                        if (properties[i].PropertyType == typeof(string))
+                        {
+                            DataType.GetProperty(properties[i].Name)
+                                .GetSetMethod()
+                                ?.Invoke(obj, new object[1] { (object)objects[i] });
+                        }
+                        else if (properties[i].PropertyType == typeof(int))
+                        {
+                            DataType.GetProperty(properties[i].Name)
+                                .GetSetMethod()
+                                ?.Invoke(obj, new object[1] { int.Parse(objects[i]) });
+                        }
+
+                    }
+                    resultList.Add((T)obj);
+                }
+
+                firstrow = false;
+            } while (row != null);
+            return (resultList.ToArray());
+
         }
 
     }
